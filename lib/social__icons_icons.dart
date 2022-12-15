3460ed89d9cadccebb1fc56fe9e0108a1cd7b42d
Saif Waheed Raja <rajasaifi49@gmail.com>
2022-12-15 18:55:59 +0500

    social icons added

---
+++ lib/social__icons_icons.dart
@@ -0,0 +1,24 @@
+///
+import 'package:flutter/widgets.dart';
+
+class SocialIcons {
+  SocialIcons._();
+
+  static const _kFontFam = 'Social_Icons';
+  static const _kFontPkg = null;
+
+  static const IconData githubCircled =
+      IconData(0xf09b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData html5 =
+      IconData(0xf13b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData css3 =
+      IconData(0xf13c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData instagram =
+      IconData(0xf16d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData twitter =
+      IconData(0xf309, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData facebookSquared =
+      IconData(0xf30e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bootstrap =
+      IconData(0xf836, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
